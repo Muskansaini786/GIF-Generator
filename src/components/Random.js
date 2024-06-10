61f2b9d1bf235b8d589b5be0c3792e71f290d656
Muskansaini786 <muskansaini455gmail.com>
2024-06-10 14:27:44 +0530

    commit

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,11 +1,5 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { useEffect } from 'react';
 import Spinner from './Spinner';
 import useGif from '../hooks/useGif';
-
-
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
 
